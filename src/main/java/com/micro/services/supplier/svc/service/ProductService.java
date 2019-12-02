9ee73b9b0d998f1b6eae8c4c7bb8841f1813020c
daviddai <david.dai.personal@gmail.com>
2019-12-02 22:23:51 +1100

    removed unused import

--- src/main/java/com/micro/services/supplier/svc/service/ProductService.java
+++ src/main/java/com/micro/services/supplier/svc/service/ProductService.java
@@ -7,7 +7,6 @@ import com.micro.services.supplier.svc.model.response.ProductApiModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
