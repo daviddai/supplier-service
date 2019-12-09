d4e7aa5fdc4bcd4533b115034cfc68233788380c
ddai <ddai@tripadvisor.com>
2019-12-09 14:41:34 +1100

    added serial version id

--- src/main/java/com/micro/services/supplier/svc/exception/SupplierServiceException.java
+++ src/main/java/com/micro/services/supplier/svc/exception/SupplierServiceException.java
@@ -2,6 +2,8 @@ package com.micro.services.supplier.svc.exception;
 
 public class SupplierServiceException extends Exception {
 
+    private static final long serialVersionUID = -1074881423330549068L;
+
     public enum ErrorCode {
         SUPPLIER_FAILED_TO_ADD_PRODUCT_TO_DB,
         SUPPLIER_FAILED_TO_FIND_NON_EXISTING_PRODUCT_CODE,
